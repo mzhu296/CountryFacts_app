45950efbd3804bad0923fd9e56e9018d1957bb79
mzhu296 <113631797+mzhu296@users.noreply.github.com>
2023-09-14 14:09:52 -0400

    searchbox

---
+++ script.js
@@ -0,0 +1,65 @@
+// Sample data (replace with your actual data)
+const countriesData = [
+    {
+        name: "United States of America",
+        currency: "United States Dollar (USD)",
+    },
+    // Add data for other countries
+];
+
+document.getElementById('nameSearchBtn').addEventListener('click', searchByName);
+document.getElementById('currencySearchBtn').addEventListener('click', searchByCurrency);
+
+function searchByName() {
+    const nameSearchValue = document.getElementById('nameSearch').value.trim();
+    if (nameSearchValue.length > 20 || /\d/.test(nameSearchValue)) {
+        alert("Invalid input for Name search.");
+        return;
+    }
+    displayResults(nameSearchValue.toLowerCase(), "name");
+}
+
+function searchByCurrency() {
+    const currencySearchValue = document.getElementById('currencySearch').value.trim();
+    if (!/^[A-Z]{3}$/.test(currencySearchValue)) {
+        alert("Invalid input for Currency search. Please enter three uppercase letters (A-Z).");
+        return;
+    }
+    displayResults(currencySearchValue, "currency");
+}
+
+function displayResults(query, type) {
+    let matches = [];
+    for (const country of countriesData) {
+        if (type === "name" && country.name.toLowerCase().includes(query)) {
+            matches.push(country);
+        } else if (type === "currency" && country.currency.toUpperCase().includes(query)) {
+            matches.push(country);
+        }
+        if (matches.length >= 5) {
+            break;
+        }
+    }
+
+    if (matches.length === 0) {
+        alert("No matches found.");
+    } else {
+        let message = "Matches found:\n";
+        for (const match of matches) {
+            message += `- ${match.name} (${match.currency})\n`;
+        }
+        alert(message);
+    }
+}
+
+document.getElementById('nameSearch').addEventListener('keyup', function (event) {
+    if (event.key === "Enter") {
+        searchByName();
+    }
+});
+
+document.getElementById('currencySearch').addEventListener('keyup', function (event) {
+    if (event.key === "Enter") {
+        searchByCurrency();
+    }
+});
