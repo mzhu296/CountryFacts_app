e41cdcaab7d8db4a086521ea32da66a9c85ad7b0
mzhu296 <113631797+mzhu296@users.noreply.github.com>
2023-10-03 13:04:36 -0400

    hide display bar when the search bar is blank

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const nameSearchInput = document.getElementById("nameSearch");
     const searchResults = document.createElement("div");
     searchResults.id = "searchResults";
+    searchResults.style.display = "none";
     document.querySelector(".search-container").appendChild(searchResults);
   
     // Event listener for input changes in the name search box
@@ -12,20 +13,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   
     function updateSearchResults(searchTerm) {
-       // Remove all child elements from the search results container
-       while (searchResults.firstChild) {
+      // Remove all child elements from the search results container
+      while (searchResults.firstChild) {
         searchResults.removeChild(searchResults.firstChild);
-       }
+      }
+      
       // Loop through the country list and add matching items to the search results
-      const countries = document.querySelectorAll(".country")
+      const countries = document.querySelectorAll(".country");
+      let hasResults = false; // Flag to check if there are any results
   
       countries.forEach(function (country) {
         const countryName = country.querySelector("h2").textContent.toLowerCase();
         if (countryName.includes(searchTerm)) {
           const clone = country.cloneNode(true);
           searchResults.appendChild(clone);
+          hasResults = true; // Set the flag to true if at least one result is found
         }
       });
+  
+      // Hide the search results container if there are no results
+      if (!hasResults) {
+        searchResults.style.display = "none";
+      } else {
+        searchResults.style.display = "block";
+      }
     }
   });
   
