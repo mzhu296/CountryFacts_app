78a84a9e19ebcc3555136d8133bb81ab2a4141c3
mzhu296 <113631797+mzhu296@users.noreply.github.com>
2023-10-05 08:32:56 -0400

    optimize the search method

--- script.js
+++ script.js
@@ -1,60 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get references to the search input and content block
-  const nameSearchInput = document.getElementById("nameSearch");
-  const currencySearchInput = document.getElementById("currencySearch");
-  const searchResults = document.createElement("div");
-  searchResults.id = "searchResults";
-  searchResults.style.display = "none";
-  document.querySelector(".search-container").appendChild(searchResults);
+    // Get references to the search input and content block
+    const searchCountry = document.getElementById("nameSearch");
+    const searchCurrency = document.getElementById("currencySearch");
+    const searchResults = document.createElement("div");
+    searchResults.id = "searchResults";
+    searchResults.style.display = "none";
+    document.querySelector(".search-container").appendChild(searchResults);
 
-  // Event listener for input changes in the name search box
-  nameSearchInput.addEventListener("input", function () {
-      const searchTerm = nameSearchInput.value.toLowerCase();
-      updateSearchResults(searchTerm, "name");
-  });
+    // Event listener for input changes in the name search box
+    searchCountry.addEventListener("input", function () {
+        const keyword = searchCountry.value.toLowerCase();
+        updateSearchResults(keyword, "name");
+    });
 
-  // Event listener for input changes in the currency code search box
-  currencySearchInput.addEventListener("input", function () {
-      const searchTerm = currencySearchInput.value.toLowerCase(); // Convert input to uppercase
-      updateSearchResults(searchTerm, "currency-code");
-  });
+    // Event listener for input changes in the currency code search box
+    searchCurrency.addEventListener("input", function () {
+        const keyword = searchCurrency.value.toUpperCase(); // Convert input to uppercase
+        updateSearchResults(keyword, "currency");
+    });
 
-  function updateSearchResults(searchTerm, searchType) {
-      // Remove all child elements from the search results container
-      while (searchResults.firstChild) {
-          searchResults.removeChild(searchResults.firstChild);
-      }
+    function updateSearchResults(keyword, searchType) {
+        // Remove all child elements from the search results container
+        while (searchResults.firstChild) {
+            searchResults.removeChild(searchResults.firstChild);
+        }
 
-      // Hide the search results container if the search term is empty
-      if (searchTerm.trim() === "") {
-          searchResults.style.display = "none";
-          return;
-      }
+        // Hide the search results container if the search term is empty
+        searchResults.style.display = keyword.trim() === "" ? "none" : "block";
 
-      // Loop through the country list and add matching items to the search results
-      const countries = document.querySelectorAll(".country");
-      let hasResults = false; // Flag to check if there are any results
+        // Loop through the country list and add matching items to the search results
+        const countries = document.querySelectorAll(".country");
 
-      countries.forEach(function (country) {
-          let textToSearch = "";
-          if (searchType === "name") {
-              textToSearch = country.querySelector("h2").textContent.toLowerCase();
-          } else if (searchType === "currency-code") {
-              textToSearch = country.querySelector("h3").textContent.toLowerCase();
-          }
+        for (let i = 0; i < countries.length; i++) {
+            const country = countries[i];
+            let textToSearch = "";
+            if (searchType === "name") {
+                textToSearch = country.querySelector("h2").textContent.toLowerCase();
+            } else if (searchType === "currency") {
+                textToSearch = country.querySelector("h3").textContent.toUpperCase();
+            }
 
-          if (textToSearch.includes(searchTerm)) {
-              const clone = country.cloneNode(true);
-              searchResults.appendChild(clone);
-              hasResults = true; // Set the flag to true if at least one result is found
-          }
-      });
-
-      // Hide the search results container if there are no results
-      if (!hasResults) {
-          searchResults.style.display = "none";
-      } else {
-          searchResults.style.display = "block";
-      }
-  }
+            if (textToSearch.includes(keyword)) {
+                const clone = country.cloneNode(true);
+                searchResults.appendChild(clone);
+            }
+        }
+    }
 });
